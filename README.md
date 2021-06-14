a1ee3182c56cfa2f51dca8709d12ed7fb6633b9e
Rafał Chłodnicki <rchl2k@gmail.com>
2021-06-14 11:33:40 -0700

    Fix installation on Windows (#2)
    Fixes #1

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # LSP-cmake
 
-This is a helper package that automatically installs and updates a
-[CMake Language Server](https://github.com/regen100/cmake-language-server) for you.
+This is a helper package that automatically installs and updates a [CMake Language Server](https://github.com/regen100/cmake-language-server) for you.
 
-To use this package, you must have the [LSP](https://packagecontrol.io/packages/LSP) package installed as well.
+### Installation
+
+* Install [LSP](https://packagecontrol.io/packages/LSP)
+* Install [CMake](https://packagecontrol.io/packages/CMake) package to enable support for CMake syntax.
+* Install [LSP-CMake](https://packagecontrol.io/packages/LSP-CMake)
+* Make sure that `cmake` is in the `PATH` and Sublime Text can find it.
+* Restart Sublime.
 
 ## Applicable Selectors
 
@@ -11,10 +16,10 @@ This language server operates on views with the `source.cmake` base scope.
 
 ## Installation Location
 
-The server is installed in the $DATA/Cache/LSP-cmake directory, where $DATA is the base data path of Sublime Text.
-For instance, $DATA is `~/.config/sublime-text` on a Linux system. If you want to force a re-installation of the server,
-you can delete the entire $DATA/Cache/LSP-cmake directory. The installation is done through a virtual environment,
-using pip. Therefore, you must have at least the `python` executable installed and it must be present in your $PATH.
+The server is installed in the `$CACHE/Package Storage/LSP-CMake` directory, where `$CACHE` is the base data path of Sublime Text.
+For instance, `$CACHE` is `~/.cache/sublime-text` on a Linux system. If you want to force a re-installation of the server,
+you can delete the entire `$CACHE/Package Storage/LSP-CMake` directory or just reinstall the package. The installation is done through a virtual environment, using
+pip. Therefore, you must have at least the `python` executable installed and it must be present in your `$PATH`.
 
 Like any helper package, installation starts when you open a view that is suitable for this language server. In this
 case, that means that when you open a view with the `source.cmake` base scope, installation commences.
--- plugin.py
+++ plugin.py
@@ -1,71 +1,22 @@
+from LSP.plugin.core.typing import Dict
+from lsp_utils.pip_client_handler import PipClientHandler
 import os
-import shutil
-import subprocess
-import re
-
 import sublime
-from LSP.plugin import AbstractPlugin
-from LSP.plugin.core.typing import Any
 
 
-class Cmake(AbstractPlugin):
+class Cmake(PipClientHandler):
+    package_name = __package__
+    requirements_txt_path = "requirements.txt"
+    server_filename = "cmake-language-server"
+
     @classmethod
-    def name(cls) -> str:
+    def get_displayed_name(cls) -> str:
         return "cmake"
 
-    @classmethod
-    def basedir(cls) -> str:
-        return os.path.join(sublime.cache_path(), "LSP-cmake")
 
-    @classmethod
-    def bindir(cls) -> str:
-        return os.path.join(cls.basedir(), "bin")
+def plugin_loaded() -> None:
+    Cmake.setup()
 
-    @classmethod
-    def server_exe(cls) -> str:
-        return os.path.join(cls.bindir(), "cmake-language-server")
 
-    @classmethod
-    def pip_exe(cls) -> str:
-        return os.path.join(cls.bindir(), "pip")
-
-    @classmethod
-    def version_str(cls) -> str:
-        settings = sublime.load_settings("LSP-cmake.sublime-settings")
-        return str(settings.get("version"))
-
-    @classmethod
-    def python_exe(cls) -> str:
-        return "python" if sublime.platform() == "windows" else "python3"
-
-    @classmethod
-    def run(cls, *args: Any, **kwargs: Any) -> bytes:
-        if sublime.platform() == "windows":
-            startupinfo = subprocess.STARTUPINFO()  # type: ignore
-            flag = subprocess.STARTF_USESHOWWINDOW  # type: ignore
-            startupinfo.dwFlags |= flag
-        else:
-            startupinfo = None
-        return subprocess.check_output(args=args, cwd=kwargs.get("cwd"), startupinfo=startupinfo)
-
-    @classmethod
-    def needs_update_or_installation(cls) -> bool:
-        if os.path.exists(cls.server_exe()):
-            first_line = cls.run(cls.server_exe(), "--version").decode("ascii").splitlines()[0]
-            match = re.search(r"cmake-language-server (.+)", first_line)
-            if match:
-                current_version_str = match.group(1)
-                return current_version_str != cls.version_str()
-        return True
-
-    @classmethod
-    def install_or_update(cls) -> None:
-        shutil.rmtree(cls.basedir(), ignore_errors=True)
-        try:
-            os.makedirs(cls.basedir(), exist_ok=True)
-            cls.run(cls.python_exe(), "-m", "venv", "LSP-cmake", cwd=sublime.cache_path())
-            cmake = "cmake-language-server=={}".format(cls.version_str())
-            cls.run(cls.pip_exe(), "install", cmake)
-        except Exception:
-            shutil.rmtree(cls.basedir(), ignore_errors=True)
-            raise
+def plugin_unloaded() -> None:
+    Cmake.cleanup()
